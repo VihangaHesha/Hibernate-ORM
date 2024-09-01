1c9317d2e2629209053b12463d105a02ac2b27a4
Vihanga Heshan Bandara <heshanvihanga321@gmail.com>
2024-09-01 10:53:09 +0530

    Modified the SessionFactory class and the Customer Entity class

--- src/lk/ijse/gdse/orm/hibernate/config/SessionFactoryConfig.java
+++ src/lk/ijse/gdse/orm/hibernate/config/SessionFactoryConfig.java
@@ -10,23 +10,48 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class SessionFactoryConfig {
     private static SessionFactoryConfig factoryConfig;
-    private SessionFactoryConfig(){}
-    public static SessionFactoryConfig getInstance(){
-        return (null == factoryConfig ) ? factoryConfig = new SessionFactoryConfig() : factoryConfig;
+
+    private SessionFactoryConfig() {
     }
-//    In here we need to import the hibernate.org package and no other
-    public Session getSession(){
+
+    public static SessionFactoryConfig getInstance() {
+        return (null == factoryConfig) ? factoryConfig = new SessionFactoryConfig() : factoryConfig;
+    }
+
+    //    In here we need to import the hibernate.org package and no other
+    public Session getSession() {
+
 //        01.Created a service registry as the step one
-        StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().configure().build();
+        StandardServiceRegistry serviceRegistry =
+                new StandardServiceRegistryBuilder()
+                        .configure()
+                        .build();
+
+        /*
+        When we changed the file path or changed the name of the xml file we have to call this
+        configure method !!!
+
+        .configure(XML file path)
+
+        */
+
 //        02.Create a MetaData Object
-        Metadata metadata = new MetadataSources(serviceRegistry).addAnnotatedClass(Customer.class).getMetadataBuilder().build();
+        Metadata metadata =
+                new MetadataSources(serviceRegistry)
+                        .addAnnotatedClass(Customer.class)
+                        .getMetadataBuilder()
+                        .build();
+
 //        03.Create a SessionFactory Object
-        SessionFactory sessionFactory = metadata.buildSessionFactory();
+        SessionFactory sessionFactory =
+                metadata
+                        .buildSessionFactory();
 
         return sessionFactory.openSession();
 
         /*
-        we can do this too instead of doing above
+
+       ---- we can do this too instead of doing above ----
 
         return metadata.buildSessionFactory().openSession();
 
--- src/lk/ijse/gdse/orm/hibernate/entity/Customer.java
+++ src/lk/ijse/gdse/orm/hibernate/entity/Customer.java
@@ -5,8 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
-@Table(name = "customer")
+@Entity(name = "customer")
+//@Table(name = "customer")
 public class Customer {
     @Id
     @Column(name = "customer_id")
